9cf554d82d5dc53f77fb00f0c85b5f373f21e672
Gavin Doughtie <gavin@doughtie.com>
2021-01-02 15:59:10 -0800

    messing around with enums; how to differentiate

--- projects/enums_and_pattern_matching/src/main.rs
+++ projects/enums_and_pattern_matching/src/main.rs
@@ -1,3 +1,20 @@
+#[derive(Debug)]
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
 fn main() {
-    println!("Hello, enums_and_pattern_matching!");
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+    route(&home);
+    route(&loopback);
+}
+
+fn route(addr: &IpAddr) {
+    println!("v4 address: {:#?}", addr);
+}
+
+fn _route(addr: &IpAddr) {
+    println!("addr: {:#?}", addr);
 }
