5884b3ac890c3e0756e2cc6a2f31f767749d730e
Gavin Doughtie <gavin.doughtie@aon.com>
2021-01-02 07:08:19 -0800

    misc code tweaks

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,15 +1,25 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
     let mut guess = String::new();
 
-    io::stdin()
+    let bytes_read = io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+    println!(
+        "Which is {str_len} long and you typed {bytes_read} bytes",
+        bytes_read = bytes_read,
+        str_len = guess.len()
+    );
 }
