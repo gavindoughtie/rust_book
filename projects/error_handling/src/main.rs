ce5f2e66ed3e3b8cdb08b81eee6e1529e7afb436
Gavin Doughtie <gavin.doughtie@aon.com>
2021-01-06 10:31:18 -0800

    error handling and file reading

--- projects/error_handling/src/main.rs
+++ projects/error_handling/src/main.rs
@@ -1,3 +1,18 @@
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
     println!("Hello, error_handling!");
+    let f = File::open("./src/hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+
+    let mut hello = String::new();
+    
+    let bytes = f.read_to_string(&mut hello).expect("Failed to read file");
+
+    print!("read {}:\n{}\n", bytes, hello);
 }
