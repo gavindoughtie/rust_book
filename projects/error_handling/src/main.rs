d476b98889b57211efd175f50d3e83dc505987e7
Gavin Doughtie <gavin.doughtie@aon.com>
2021-01-08 20:18:58 -0800

    more error handling code golf

--- projects/error_handling/src/main.rs
+++ projects/error_handling/src/main.rs
@@ -10,7 +10,8 @@ fn main() {
         Err(error) => match error.kind() {
             ErrorKind::NotFound => match File::create(filename) {
                 Ok(mut fc) => {
-                    fc.write_fmt(format_args!("Welcome to {}\n", filename)).expect("Failed to write");
+                    fc.write_fmt(format_args!("Welcome to {}\n", filename))
+                        .expect("Failed to write");
                     let fc = File::open(filename).expect("failed to re-open new file");
                     fc
                 }
@@ -29,6 +30,11 @@ fn main() {
     let compact_filename = format!("compact_{}", filename);
     let compact = open_file_compact(&compact_filename).unwrap();
     println!("open_file_compact read: {}", &compact);
+
+    let more_compact_filename = format!("more_compact_{}", filename);
+    let more_compact =
+        open_file_more_compact(&more_compact_filename).expect("failed to read via more_compact");
+    println!("open_file_more_compact read: {}", &more_compact);
 }
 
 fn open_file_compact(filename: &str) -> Result<String, Error> {
@@ -37,7 +43,8 @@ fn open_file_compact(filename: &str) -> Result<String, Error> {
             let mut fc = File::create(filename).unwrap_or_else(|error| {
                 panic!(format!("Problem creating {} {:?}", filename, error));
             });
-            fc.write_fmt(format_args!("file_compact wrote {}", filename)).expect("failed to write to file after creating it");
+            fc.write_fmt(format_args!("file_compact wrote {}", filename))
+                .expect("failed to write to file after creating it");
             let fc = File::open(filename).expect("failed to read file after creating it");
             fc
         } else {
@@ -47,8 +54,23 @@ fn open_file_compact(filename: &str) -> Result<String, Error> {
     let mut output = String::new();
     match f.read_to_string(&mut output) {
         Ok(_) => Ok(output),
-        Err(e) => Err(e)
+        Err(e) => Err(e),
     }
-    // .expect(&format!("Failed to read {}", filename));
-    // return output;
+}
+
+fn open_file_more_compact(filename: &str) -> Result<String, Error> {
+    let mut f = File::open(filename).unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            let mut fc = File::create(filename).unwrap_or_else(|error| {
+                panic!(format!("Problem creating {} {:?}", filename, error));
+            });
+            fc.write_fmt(format_args!("file_compact wrote {}", filename)).expect("Failed to write new file");
+            File::open(filename).unwrap()
+        } else {
+            panic!("Problem opening {} {:?}", filename, error);
+        }
+    });
+    let mut output = String::new();
+    f.read_to_string(&mut output)?;
+    Ok(output)
 }
