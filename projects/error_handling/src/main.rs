39c1cf32225ca6d68a4b2ff7e93d15b38542dea3
Gavin Doughtie <gavin.doughtie@aon.com>
2021-01-08 18:30:24 -0800

    random gold plating of file handling code

--- projects/error_handling/src/main.rs
+++ projects/error_handling/src/main.rs
@@ -1,18 +1,17 @@
 use std::fs::File;
-use std::io::{ErrorKind, Read};
+use std::io::{ErrorKind, Read, Write};
 
 fn main() {
-    let filename = "hello4.txt";
-
-    // let mut f = File::open(filename).expect("couldn't open");
-
+    let args: Vec<String> = std::env::args().collect();
+    println!("{:?}", args);
+    let filename = &args[1];
     let mut f = match File::open(filename) {
         Ok(file) => file,
         Err(error) => match error.kind() {
             ErrorKind::NotFound => match File::create(filename) {
-                Ok(fc) => {
-                    // fc.write_str("Welcome to ");
-                    // fc.write_str(filename);
+                Ok(mut fc) => {
+                    fc.write_fmt(format_args!("Welcome to {}\n", filename)).expect("Failed to write");
+                    let fc = File::open(filename).expect("failed to re-open new file");
                     fc
                 }
                 Err(e) => panic!("Problem creating the file: {:?}", e),
@@ -27,4 +26,25 @@ fn main() {
     let bytes = f.read_to_string(&mut hello).expect("Failed to read file");
 
     print!("read {}, {} bytes:\n{}\n", filename, bytes, hello);
+    let compact_filename = format!("compact_{}", filename);
+    let compact = open_file_compact(&compact_filename);
+    println!("open_file_compact read: {}", &compact);
+}
+
+fn open_file_compact(filename: &str) -> String {
+    let mut f = File::open(filename).unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            let mut fc = File::create(filename).unwrap_or_else(|error| {
+                panic!(format!("Problem creating {} {:?}", filename, error));
+            });
+            fc.write_fmt(format_args!("file_compact wrote {}", filename)).expect("failed to write to file after creating it");
+            let fc = File::open(filename).expect("failed to read file after creating it");
+            fc
+        } else {
+            panic!("Problem opening {} {:?}", filename, error);
+        }
+    });
+    let mut output = String::new();
+    f.read_to_string(&mut output).expect(&format!("Failed to read {}", filename));
+    return output;
 }
