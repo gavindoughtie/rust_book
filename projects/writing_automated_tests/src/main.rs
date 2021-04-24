b2f862f1c9d3c8b36f5849252b904c261ba5d54f
Gavin Doughtie <gavin.doughtie@aon.com>
2021-04-24 12:44:18 -0700

    fooling aroun with modular tests

--- projects/writing_automated_tests/src/main.rs
+++ projects/writing_automated_tests/src/main.rs
@@ -1,3 +1,16 @@
+mod adder;
+mod rect;
+
 fn main() {
   println!("automated tests");
+}
+
+#[cfg(test)]
+mod tests {
+
+  #[test]
+  fn exploration() {
+    println!("tests in main.rs");
+    assert_eq!(2 + 2, 4);
+  }
 }
---
+++ projects/writing_automated_tests/src/rect/tests.rs
@@ -0,0 +1,34 @@
+use super::*;
+
+#[cfg(test)]
+mod tests {
+  use rect::*;
+
+  #[test]
+  fn larger_can_hold_smaller() {
+    let larger = Rectangle {
+      width: 8,
+      height: 7,
+    };
+    let smaller = Rectangle {
+      width: 5,
+      height: 1,
+    };
+
+    assert!(larger.can_hold(&smaller));
+  }
+
+  #[test]
+  fn smaller_cannot_hold_larger() {
+    let larger = Rectangle {
+      width: 8,
+      height: 7,
+    };
+    let smaller = Rectangle {
+      width: 5,
+      height: 1,
+    };
+
+    assert!(!smaller.can_hold(&larger));
+  }
+}
