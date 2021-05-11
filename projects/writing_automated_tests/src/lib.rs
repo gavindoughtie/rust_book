0a32d779196d2d91caa73ab7929b3b49990e4f03
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-11 07:41:41 -0700

    removed stub

--- projects/writing_automated_tests/src/lib.rs
+++ projects/writing_automated_tests/src/lib.rs
@@ -1,14 +1,2 @@
 mod adder;
 mod rect;
-
-#[cfg(test)]
-mod tests {
-
-  #[test]
-  fn exploration() {
-    println!("tests in main.rs\n");
-    println!("#######################\n");
-    assert_eq!(2 + 2, 4);
-  }
-
-}
