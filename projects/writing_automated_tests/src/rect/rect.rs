7cdfd2c015aaf23aaa03f0bbd01608c53027fe6c
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-12 07:50:02 -0700

    integration tests

--- projects/writing_automated_tests/src/rect/rect.rs
+++ projects/writing_automated_tests/src/rect/rect.rs
@@ -1,12 +1,46 @@
 
 #[derive(Debug)]
 pub struct Rectangle {
-  width: u32,
-  height: u32,
+  pub width: u32,
+  pub height: u32,
 }
 
 impl Rectangle {
   pub fn can_hold(&self, other: &Rectangle) -> bool {
     self.width > other.width && self.height > other.height
+  }
+  pub fn get_height(&self) -> u32 {
+    return self.height;
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn larger_can_hold_smaller() {
+    let larger = super::Rectangle {
+      width: 8,
+      height: 7,
+    };
+    let smaller = super::Rectangle {
+      width: 5,
+      height: 1,
+    };
+
+    assert!(larger.can_hold(&smaller));
+  }
+
+  #[test]
+  fn smaller_cannot_hold_larger() {
+    let larger = super::Rectangle {
+      width: 8,
+      height: 7,
+    };
+    let smaller = super::Rectangle {
+      width: 5,
+      height: 1,
+    };
+
+    assert!(!smaller.can_hold(&larger));
   }
 }
--- projects/writing_automated_tests/src/tests/integration_test.rs
+++
@@ -1,6 +0,0 @@
-mod adder;
-
-#[test]
-fn it_adds_two() {
-  assert_eq!(4, adder::add_two(2));
-}
