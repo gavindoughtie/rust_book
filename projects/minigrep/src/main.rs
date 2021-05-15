3dae06d185e0cf0d3abd6a8f9daf71f36508e553
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-15 09:37:18 -0700

    error handling of argument parsing

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
@@ -7,30 +8,27 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config {query, filename}
+        Ok(Config { query, filename })
     }
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config {query, filename}
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 3 {
-        let config = Config::new(&args);
-        println!("searching for {} in {}", config.query, config.filename);
-        let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-        println!("With text:\n{}", contents);
-    } else {
+    let config = Config::new(&args).unwrap_or_else(|err | {
+        println!("Problem parsing arguments: {}", err);
         usage();
-    }
+        process::exit(1);
+    });
+    println!("searching for {} in {}", config.query, config.filename);
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    println!("With text:\n{}", contents);
 }
 
 fn usage() {
