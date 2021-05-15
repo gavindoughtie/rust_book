67042f5237325127c22b7d9924089b302a16081f
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-15 08:41:45 -0700

    12.2 reading file contents

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,11 +1,14 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() == 3 {
         let query = &args[1];
         let filename = &args[2];
-        println!("searching for {} in {}", query, filename)
+        println!("searching for {} in {}", query, filename);
+        let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+        println!("With text:\n{}", contents);
     } else {
         usage();
     }
