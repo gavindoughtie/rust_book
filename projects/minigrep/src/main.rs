586251c2937338fb5f3ea4e9693649372a6ca0bc
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-15 08:31:01 -0700

    reads command line arguments

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 3 {
+        let query = &args[1];
+        let filename = &args[2];
+        println!("searching for {} in {}", query, filename)
+    } else {
+        usage();
+    }
+}
+
+fn usage() {
+    println!("Usage: minigrep query filename(s)");
 }
