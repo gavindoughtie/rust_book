30c12838671db0fd2f091ff2958c05efda08b848
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-15 09:29:47 -0700

    moving parsing to config constructor

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,13 +1,32 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config {query, filename}
+    }
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config {query, filename}
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() == 3 {
-        let query = &args[1];
-        let filename = &args[2];
-        println!("searching for {} in {}", query, filename);
-        let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+        let config = Config::new(&args);
+        println!("searching for {} in {}", config.query, config.filename);
+        let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
         println!("With text:\n{}", contents);
     } else {
         usage();
