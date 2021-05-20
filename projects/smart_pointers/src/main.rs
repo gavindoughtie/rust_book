0454006fc70b28e27caf15e90181e97c9206d358
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-20 14:34:37 -0700

    cons list hacking

--- projects/smart_pointers/src/main.rs
+++ projects/smart_pointers/src/main.rs
@@ -1,3 +1,22 @@
+use crate::List::{Cons, Nil};
+
+#[derive(Debug)]
+enum List<T> {
+    Cons(T, Box<List<T>>),
+    Nil,
+}
+
+impl List<T> {
+  fn toString() -> String {
+    return self::Cons
+  }
+
+  fn get() -> T {
+      return self::Cons[0]
+  }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let list = Cons("foo", Box::new(Cons("bar", Box::new(Cons("baz", Box::new(Nil))))));
+    println!("list: {:?}", list);
 }
