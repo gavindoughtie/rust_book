46a4b624e3eeb163ef358981105aab7805521f9d
Gavin Doughtie <gavin.doughtie@aon.com>
2021-05-20 21:11:41 -0700

    yay format works

--- projects/smart_pointers/src/main.rs
+++ projects/smart_pointers/src/main.rs
@@ -1,24 +1,31 @@
 use crate::List::{Cons, Nil};
+use std::fmt;
 
 #[derive(Debug)]
-enum List<T> {
+enum List<T: Default+Copy+std::fmt::Display> {
     Cons(T, Box<List<T>>),
     Nil,
 }
 
-impl<T> List<T> {
-  fn toString() -> String {
-    String::from("???")
+impl<T: Default+Copy+std::fmt::Display> List<T> {
+  fn to_string(&self) -> String {
+      match self {
+          List::Cons(t, b) => format!("{} {}", t, b.to_string()),
+          List::Nil => String::new()
+      }
   }
+}
 
-  fn get(&self) -> T {
-    match self {
-      List::Cons(t, b) => T
-    }
+impl<T: Default+Copy+std::fmt::Display> fmt::Display for List<T> {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      // The `f` value implements the `Write` trait, which is what the
+      // write! macro is expecting. Note that this formatting ignores the
+      // various flags provided to format strings.
+      write!(f, "{}", self.to_string())
   }
 }
 
 fn main() {
     let list = Cons("foo", Box::new(Cons("bar", Box::new(Cons("baz", Box::new(Nil))))));
-    println!("list: {:?}", list);
+    println!("list: {}", list);
 }
