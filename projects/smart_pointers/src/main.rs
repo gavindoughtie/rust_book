18ca0a50186ad70a77f034eb0b636e1563105d28
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-22 18:11:17 -0700

    15.3 Drop

--- projects/smart_pointers/src/main.rs
+++ projects/smart_pointers/src/main.rs
@@ -3,33 +3,35 @@ use std::fmt;
 use std::ops::Deref;
 
 #[derive(Debug)]
-enum List<T: Default+Copy+std::fmt::Display> {
+enum List<T: Default + Copy + std::fmt::Display> {
     Cons(T, Box<List<T>>),
     Nil,
 }
 
-impl<T: Default+Copy+std::fmt::Display> List<T> {
-  fn to_string(&self) -> String {
-      match self {
-          List::Cons(t, b) => format!("{} {}", t, b.to_string()),
-          List::Nil => String::new()
-      }
-  }
+impl<T: Default + Copy + std::fmt::Display> List<T> {
+    fn to_string(&self) -> String {
+        match self {
+            List::Cons(t, b) => format!("{} {}", t, b.to_string()),
+            List::Nil => String::new(),
+        }
+    }
 }
 
-impl<T: Default+Copy+std::fmt::Display> fmt::Display for List<T> {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-      // The `f` value implements the `Write` trait, which is what the
-      // write! macro is expecting. Note that this formatting ignores the
-      // various flags provided to format strings.
-      write!(f, "{}", self.to_string())
-  }
+impl<T: Default + Copy + std::fmt::Display> fmt::Display for List<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // The `f` value implements the `Write` trait, which is what the
+        // write! macro is expecting. Note that this formatting ignores the
+        // various flags provided to format strings.
+        write!(f, "{}", self.to_string())
+    }
 }
 
 fn main() {
-    let list = Cons("foo", Box::new(Cons("bar", Box::new(Cons("baz", Box::new(Nil))))));
+    let list = Cons(
+        "foo",
+        Box::new(Cons("bar", Box::new(Cons("baz", Box::new(Nil))))),
+    );
     println!("list: {}", list);
-
 
     // deref examples:
     let x = 5;
@@ -44,7 +46,32 @@ fn main() {
 
     let m = MyBox::new(String::from("Rust"));
     hello(&m);
-    hello(&"gavin"[1..5])
+    hello(&"gavin"[1..5]);
+
+    println!("outside of smart pointer scope");
+    {
+        let c = CustomSmartPointer {
+            data: String::from("my stuff"),
+        };
+        let d = CustomSmartPointer {
+            data: String::from("other stuff"),
+        };
+        let e = CustomSmartPointer { data: String::from("e pointer")};
+        println!("custom smart pointers {} and {}, {}", c.data, d.data, e.data);
+        drop(d);
+    }
+    println!("after CustomSmartPointers scope.");
+}
+
+#[derive(Debug)]
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
 }
 
 fn hello(name: &str) {
