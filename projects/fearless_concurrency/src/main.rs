e86286703a7c83cd274fa4add9345961df6c990c
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-23 16:22:42 -0700

    16.1 moving values into thread closures, join handle

--- projects/fearless_concurrency/src/main.rs
+++ projects/fearless_concurrency/src/main.rs
@@ -1,3 +1,25 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!, vec: {:?}", i, v);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    // This is now an error, since v is moved to the thread:
+    // println!("v after threads: {:?}", v);
+
+    handle.join().unwrap();
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // println!("handle {:?}", handle);
 }
