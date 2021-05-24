b70526db96fc689df18a16fdb3d91799e4881cd9
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-23 17:09:13 -0700

    16.2 messaging

--- projects/fearless_concurrency/src/main.rs
+++ projects/fearless_concurrency/src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
     let v = vec![1, 2, 3];
@@ -21,5 +22,52 @@ fn main() {
         thread::sleep(Duration::from_millis(1));
     }
 
-    // println!("handle {:?}", handle);
+    // Channels:
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+        // Since val has transfered ownership to the channel,
+        // the line below won't compile
+        // println!("val is {}", val);
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+
+    // open a new channel
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi1"),
+            String::from("from1"),
+            String::from("the1"),
+            String::from("thread1"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1500));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
