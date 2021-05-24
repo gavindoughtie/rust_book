77849a6470462494789e7e709ddc54a82099db9f
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-23 19:49:54 -0700

    17.2 trait object dyn polymorphism

--- projects/oo_programming/src/main.rs
+++ projects/oo_programming/src/main.rs
@@ -1,3 +1,14 @@
+use oo_programming::{AveragedCollection, Button, List, Screen};
+
 fn main() {
-    println!("Hello, world!");
+    let mut ac = AveragedCollection::new();
+    ac.add(100);
+    ac.add(200);
+    println!("average: {}", ac.average());
+
+    let b1 = Box::new(Button{label: String::from("b1"), height: 20, width: 100});
+    let b2 = Box::new(Button{label: String::from("b2"), height: 20, width: 100});
+    let l1 = Box::new(List{items: vec![String::from("item1")], height: 200, width: 100});
+    let screen = Screen{components: vec![b1, l1, b2]};
+    screen.run();
 }
