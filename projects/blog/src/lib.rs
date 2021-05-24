7d06d2eec54371109fb40ddc184eb28a6d4527e0
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-23 20:58:27 -0700

    added todo for further coding

--- projects/blog/src/lib.rs
+++ projects/blog/src/lib.rs
@@ -49,6 +49,13 @@ impl State for Published {
     }
 }
 
+/*
+    Add a reject method that changes the post’s state from PendingReview back to Draft.
+    Require two calls to approve before the state can be changed to Published.
+    Allow users to add text content only when a post is in the Draft state.
+        Hint: have the state object responsible for what might change about
+              the content but not responsible for modifying the Post.
+*/
 impl Post {
     pub fn new() -> Self {
         Post {
