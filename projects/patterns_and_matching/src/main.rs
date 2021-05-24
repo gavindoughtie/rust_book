aae8fa12467bffd13dfb9b6e7bdb4b492cd213ef
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-24 08:38:48 -0700

    18.3 destructuring in matches

--- projects/patterns_and_matching/src/main.rs
+++ projects/patterns_and_matching/src/main.rs
@@ -63,6 +63,90 @@ fn main() {
 
     println!("at the end: x = {:?}, y = {:?}", x, y);
 
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    let x = 5;
+
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 't';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    let p = Point { x: 0, y: 7 };
+
+    // verbose syntax for remapping
+    let Point { x: a, y: b } = p;
+    println!("a: {}, b: {}", a, b);
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+
+    // compact syntax when the field names match
+    // the variable names:
+    let Point {x, y} = p;
+    println!("x: {}, y: {}", x, y);
+
+    let p = Point { x: 0, y: 7 };
+
+    // Match on literal values
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+
+    let msg = Message::ChangeColor(0, 160, 255);
+    print_msg(msg);
+    let msg = Message::Quit;
+    print_msg(msg);
+    let msg = Message::Move{x: 5, y: 10};
+    print_msg(msg);
+    let msg = Message::Write(String::from("Hello world"));
+    print_msg(msg);
+}
+
+fn print_msg(msg: Message) {
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.")
+        }
+        Message::Move { x, y } => {
+            println!(
+                "Move in the x direction {} and in the y direction {}",
+                x, y
+            );
+        }
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => println!(
+            "Change the color to red {}, green {}, and blue {}",
+            r, g, b
+        ),
+    }
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+struct Point {
+    x: i32,
+    y: i32,
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
