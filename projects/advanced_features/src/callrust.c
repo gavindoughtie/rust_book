e2432ffbd550bf8db426e651583380999aa360c4
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-25 14:11:13 -0700

    19.1 unsafe rust

---
+++ projects/advanced_features/src/callrust.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+  call_from_c();
+  return 0;
+}
--- projects/advanced_features/src/main.rs
+++ projects/advanced_features/src/main.rs
@@ -1,3 +1,107 @@
+static HELLO_WORLD: &str = "Hello, world!";
+
+static mut COUNTER: u32 = 0;
+
+fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
+}
+
+fn count() -> u32 {
+    let count: u32;
+    unsafe {
+        count = COUNTER
+    }
+    return count;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}, {:?}", HELLO_WORLD, count());
+    let mut num = 5u32;
+
+    let r1 = &num as *const u32;
+    let r2 = &mut num as *mut u32;
+    let r3: u32;
+    add_to_count(1);
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+        *r2 = 0xdeadbeef;
+        r3 = *r1;
+    }
+
+    println!("r3 is {}, 0x{:x}", r3, r3);
+    unsafe {
+        dangerous();
+    }
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3);
+    add_to_count(1);
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+    println!("a {:?} b {:?}", a, b);
+    println!("{}, counter: {:?}", HELLO_WORLD, count());
+
+    let (c, d) = split_at_mut(r, 3);
+    println!("c {:?}, d {:?}", c, d);
+
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
+
+    let my_union = MyUnion{f1: 5};
+    unsafe {
+        println!("my_union f1 {}", my_union.f1);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn call_from_c() {
+    println!("Just called a Rust function from C!");
+}
+
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+    use std::slice;
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
+unsafe fn dangerous() {
+    println!("but not serious");
+    let mut num = 8u32;
+    let r1 = &num as *const u32;
+    let r2 = &mut num as *mut u32;
+    println!("r1 is: {}", *r1);
+    println!("r2 is: {}", *r2);
+}
+
+unsafe trait Foo {
+    // methods go here
+}
+
+unsafe impl Foo for i32 {
+    // method implementations go here
+}
+
+#[repr(C)]
+union MyUnion {
+    f1: u32,
+    f2: f32,
 }
