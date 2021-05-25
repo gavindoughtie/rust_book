785eade5ebaa215db51bcd823e3e5646d0d643f0
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-25 15:39:21 -0700

    19.2 Advanced Traits

--- projects/advanced_features/src/main.rs
+++ projects/advanced_features/src/main.rs
@@ -1,3 +1,22 @@
+use std::ops::Add;
+
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point; // For overloading add
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
 static HELLO_WORLD: &str = "Hello, world!";
 
 static mut COUNTER: u32 = 0;
@@ -16,7 +35,27 @@ fn count() -> u32 {
     return count;
 }
 
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
 fn main() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+
+    let m = Millimeters(3);
+    let mm = m + Meters(1);
+    assert_eq!(1003, mm.0);
+
     println!("{}, {:?}", HELLO_WORLD, count());
     let mut num = 5u32;
 
@@ -63,7 +102,44 @@ fn main() {
     for b in bar {
         println!("b: {}", b)
     }
+
+    let gavin = Human{};
+    println!("Gavin fly!");
+    gavin.fly();
+    Wizard::fly(&gavin);
+    Pilot::fly(&gavin);
+
+    println!("my dog is called a {}", Dog::baby_name());
+    println!("A baby dog is called {}", <Dog as Animal>::baby_name());
+    let s = "barfoo".to_string(); // String::from("foobar");
+    s.outline_print();
+    let wrapped = Wrapper(vec!["thing1".to_string(), "thing2".to_string()]);
+    println!("wrapped: {}", wrapped);
 }
+
+use std::fmt;
+
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "-->[{}]", self.0.join(", "))
+    }
+}
+
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
+
+impl OutlinePrint for String {}
 
 #[no_mangle]
 pub extern "C" fn call_from_c() {
@@ -142,5 +218,51 @@ impl std::iter::Iterator for Bar {
             result = None;
         }
         result
+    }
+}
+
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
     }
 }
