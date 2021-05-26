b272730615d19eee8dc5e5cfdc09ca8a10a0df10
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-25 18:12:49 -0700

    19.4 Advanced functions and closures

--- projects/advanced_features/src/main.rs
+++ projects/advanced_features/src/main.rs
@@ -133,6 +133,48 @@ fn main() {
     let f: Thunk = Box::new(|| println!("hi"));
 
     f();
+
+    let fnptr_result = do_twice(add_one, 5);
+    println!("fnptr_result {}", fnptr_result);
+
+    let list_of_numbers = vec![1, 2, 3, 99];
+    let list_of_strings: Vec<String> =
+        list_of_numbers.iter().map(ToString::to_string).collect();
+    println!("{}", Wrapper(list_of_strings));
+
+    let list_of_statuses: Vec<Status> = (0u32..20).map(Status::Value).collect();
+
+    println!("{:?},{:?}", list_of_statuses, Status::Stop);
+
+    let from_closure = returns_closure();
+    println!("from_closure: {}", from_closure(5));
+    
+}
+
+// Does not compile:
+// fn returns_closure() -> dyn Fn(i32) -> i32 {
+//     |x| x + 1
+// }
+
+// But you have to box the type
+fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
+
+#[derive(Debug)]
+enum Status {
+    Value(u32),
+    Stop,
+}
+
+// Function pointer
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
 }
 
 // trait aliases are experimental:
