87e0f5abddb09cd02c66760b10cfafccdd15fb1b
gavin.doughtie <gavin.doughtie@gmail.com>
2021-06-26 16:58:33 -0700

    added 404

--- projects/web_server/hello/src/main.rs
+++ projects/web_server/hello/src/main.rs
@@ -15,14 +15,20 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
+
     let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
         contents.len(),
         contents
     );
