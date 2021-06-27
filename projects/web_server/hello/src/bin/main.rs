93ba48c607d739c681d6749003ed3cab1a2f0158
gavin.doughtie <gavin.doughtie@gmail.com>
2021-06-27 13:07:10 -0700

    multi threaded serving works

--- projects/web_server/hello/src/bin/main.rs
+++ projects/web_server/hello/src/bin/main.rs
@@ -2,23 +2,38 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs;
+use std::str;
+use std::thread;
+use std::time::Duration;
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
+    let buffer_string = str::from_utf8(&buffer).unwrap();
+    println!("{}", buffer_string);
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        println!("sleeping for 5 seconds...");
+        thread::sleep(Duration::from_secs(5));
+        println!("awaking refreshed!");
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
